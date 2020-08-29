2c08b88cf48f92fc9575b65efb75eedc969e3f7f
chi <jfhfjj441@gmail.com>
2020-08-29 17:09:57 +0800

    (高雄)【最終試煉】JavaScript 入門篇 - 學徒的試煉(改1)

--- js/all.js
+++ js/all.js
@@ -1,69 +1,69 @@
-let data
-let len
-let xhr = new XMLHttpRequest
-xhr.open('GET', 'https://raw.githubusercontent.com/hexschool/KCGTravel/master/datastore_search.json', true)
-xhr.send(null)
+let data;
+let len;
+let xhr = new XMLHttpRequest;
+xhr.open('GET', 'https://raw.githubusercontent.com/hexschool/KCGTravel/master/datastore_search.json', true);
+xhr.send(null);
 xhr.onload = function() {
-    data = JSON.parse(xhr.responseText).result.records
-    len = data.length
-}
+    data = JSON.parse(xhr.responseText).result.records;
+    len = data.length;
+};
 
 window.onload = function() {
 
-    const select = document.querySelector('#area')
-    const content = document.querySelector('.areadata')
-    const hotarea = document.querySelector('.select')
-    const pageNumberID = document.getElementById('pagenumber')
-    const topicon = document.querySelector('.btn')
-    let thisPage = 0
-    let removeStyle = 1
-    let newData = []
-    addSelect()
+    const select = document.querySelector('#area');
+    const content = document.querySelector('.areadata');
+    const hotarea = document.querySelector('.select');
+    const pageNumberID = document.getElementById('pagenumber');
+    const topicon = document.querySelector('.btn');
+    let thisPage = 0;
+    let removeStyle = 1;
+    let newData = [];
+    addSelect();
 
     //add選單選項
     function addSelect() {
-        var str = searchData(select.value)
-        var selectData = '<option value="請選擇">--請選擇行政區--</option>'
+        var str = searchData(select.value);
+        var selectData = '<option value="請選擇">--請選擇行政區--</option>';
         for (let i = 0; i < str[0]; i++) {
-            selectData += '<option value="' + str[1][i] + '">' + str[1][i] + '</option>'
+            selectData += '<option value="' + str[1][i] + '">' + str[1][i] + '</option>';
         }
-        select.innerHTML = selectData
-        content.previousSibling.previousSibling.textContent = 'All'
-    }
+        select.innerHTML = selectData;
+        content.previousSibling.previousSibling.textContent = 'All';
+    };
 
     hotarea.addEventListener('click', function(e) {
         if (e.target.nodeName == 'A') {
-            searchData(e.target.textContent)
+            searchData(e.target.textContent);
         }
 
-    }, false)
+    }, false);
 
     select.addEventListener('change', function(e) {
-        searchData(e.target.value)
-    }, false)
+        searchData(e.target.value);
+    }, false);
 
     //search All data
     function searchData(name) {
-        newData = []
-        var str = []
-        var name = name
-        thisPage = 0
-        removeStyle = 1
+        newData = [];
+        var str = [];
+        var name = name;
+        thisPage = 0;
+        removeStyle = 1;
         for (let i = 0; i < len; i++) {
             if (data[i].Zone == name) {
-                content.previousSibling.previousSibling.textContent = name
-                newData.push(data[i])
+                content.previousSibling.previousSibling.textContent = name;
+                newData.push(data[i]);
             } else if (name == '請選擇' || name == '') {
-                content.previousSibling.previousSibling.textContent = 'All'
+                content.previousSibling.previousSibling.textContent = 'All';
                 newData.push(data[i])
-            }
+            };
             if (name == '' && str.indexOf(data[i].Zone) < 0) {
-                str.push(data[i].Zone)
+                str.push(data[i].Zone);
             }
-        }
-        printNewData()
-        page()
-        return [str.length, str]
+        };
+        printNewData();
+        page();
+        return [str.length, str];
 
 
 
@@ -71,11 +71,11 @@ window.onload = function() {
 
     // bottom page number 
     function page() {
-        let printData = newData
-        let printlen = printData.length
-        let pageNumber = Math.ceil(printlen / 10)
-        let pageStyle = pageNumberID.childNodes
-        let eleStr = `<li class="prev">prev</li>`
+        let printData = newData;
+        let printlen = printData.length;
+        let pageNumber = Math.ceil(printlen / 10);
+        let pageStyle = pageNumberID.childNodes;
+        let eleStr = `<li class="prev">prev</li>`;
 
         if (pageNumber > 1) {
             for (let i = 1; i <= pageNumber; i++) {
@@ -86,53 +86,53 @@ window.onload = function() {
             pageStyle[1].classList.add('red')
         } else {
             pageNumberID.innerHTML = ''
-        }
+        };
 
     }
 
     //change page style 
     function addStyle(e) {
-        let pageStyle = pageNumberID.childNodes
-        let pageStylelen = pageStyle.length
-        let printData = newData
-        let printlen = printData.length
-        let pageNumber = Math.ceil(printlen / 10)
-        let thisNumber = thisPage / 10
+        let pageStyle = pageNumberID.childNodes;
+        let pageStylelen = pageStyle.length;
+        let printData = newData;
+        let printlen = printData.length;
+        let pageNumber = Math.ceil(printlen / 10);
+        let thisNumber = thisPage / 10;
         if (e.target.nodeName == 'LI') {
             //prev style
             if (thisPage > 10) {
-                pageStyle[0].classList.add('blue')
+                pageStyle[0].classList.add('blue');
             } else {
-                pageStyle[0].classList.remove('blue')
+                pageStyle[0].classList.remove('blue');
             }
             //next style
             if (thisNumber == pageNumber) {
-                pageStyle[pageStylelen - 1].classList.remove('blue')
+                pageStyle[pageStylelen - 1].classList.remove('blue');
             } else {
-                pageStyle[pageStylelen - 1].classList.add('blue')
+                pageStyle[pageStylelen - 1].classList.add('blue');
             }
             //number style
             if (thisNumber !== pageStyle[removeStyle]) {
-                pageStyle[removeStyle].classList.remove('red')
-                pageStyle[1].classList.remove('red')
-                pageStyle[thisNumber].classList.add('red')
-                removeStyle = thisNumber
+                pageStyle[removeStyle].classList.remove('red');
+                pageStyle[1].classList.remove('red');
+                pageStyle[thisNumber].classList.add('red');
+                removeStyle = thisNumber;
             }
 
-        }
+        };
     }
 
     //print data 
     function printNewData(i, j) {
-        let printData = newData
-        let printlen = printData.length
-        let eleStr = ''
-        var i = i || 0
-        var j = j || 10
-        thisPage = j
+        let printData = newData;
+        let printlen = printData.length;
+        let eleStr = '';
+        var i = i || 0;
+        var j = j || 10;
+        thisPage = j;
         if (j > printlen) {
-            j = printlen
-            thisPage = Math.ceil(printlen / 10) * 10
+            j = printlen;
+            thisPage = Math.ceil(printlen / 10) * 10;
         }
         for (; i < j; i++) {
             var addele = `
@@ -155,41 +155,41 @@ window.onload = function() {
                     <p>${printData[i].Ticketinfo}</p>
                 </div>
             </div>
-        </li>`
+        </li>`;
 
-            eleStr += addele
+            eleStr += addele;
         }
         if (eleStr !== '') {
-            content.innerHTML = eleStr
+            content.innerHTML = eleStr;
         }
 
     }
 
     pageNumberID.addEventListener('click', function(e) {
-        var i = 0
-        var j = 0
+        var i = 0;
+        var j = 0;
         if (e.target.dataset.num !== undefined) {
-            j = 10 * e.target.textContent
-            i = j - 10
-            thisPage = j
+            j = 10 * e.target.textContent;
+            i = j - 10;
+            thisPage = j;
         } else if (e.target.textContent == 'next') {
             if (thisPage == 0) {
-                i = 10
-                j = 20
+                i = 10;
+                j = 20;
             } else {
-                i = thisPage
-                j = thisPage + 10
+                i = thisPage;
+                j = thisPage + 10;
             }
         } else if (e.target.textContent == 'prev') {
             if (thisPage > 10) {
-                i = thisPage - 20
-                j = thisPage - 10
-                thisPage = j
+                i = thisPage - 20;
+                j = thisPage - 10;
+                thisPage = j;
             }
         }
-        printNewData(i, j)
-        addStyle(e)
-    }, false)
+        printNewData(i, j);
+        addStyle(e);
+    }, false);
 
     topicon.addEventListener('click', function(e) {
         window.scroll({
@@ -197,5 +197,5 @@ window.onload = function() {
             left: 0,
             behavior: 'smooth'
         });
-    }, false)
+    }, false);
 };
